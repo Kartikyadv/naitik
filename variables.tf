39c702dc7995816ccc5fba28ecf562184560ec56
Kartik Yadav <ky5986390@gmail.com>
2025-07-11 18:32:06 +0530

    Auto-merged PR by n8n

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "key_name" {
+  description = "Name of the key pair to be used"
+}
+
+variable "region" {
+  description = "AWS Region where resources will be created"
+  default     = "us-east-1"
+}
