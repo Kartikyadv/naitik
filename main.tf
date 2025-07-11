03160dcdc91dcd021a4cb11d7eca02926c387624
Kartik Yadav <ky5986390@gmail.com>
2025-07-11 18:31:11 +0530

    Push Terraform files 🚀

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95ca45a2" # This should be replaced with the latest Amazon Linux 2 AMI ID
+  instance_type = "t2.micro"
+}
